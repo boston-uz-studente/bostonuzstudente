498edb1456a591b4093ef8037b8190b618b52183
Alek Stevanovic <01alek@gmail.com>
2025-02-25 23:26:32 -0500

    video modal

---
+++ js/video-modal.js
@@ -0,0 +1,17 @@
+document.getElementById('close-modal').addEventListener('click', function() {
+    document.getElementById('video').pause();
+    document.getElementById('video-modal').classList.add('hidden');
+});
+
+document.querySelectorAll('[data-modal]').forEach(modal => {
+    modal.addEventListener('click', function() {
+        document.getElementById(modal.dataset.modal).classList.remove('hidden');
+        document.getElementById('video').play();
+    });
+});
+
+document.getElementById('video-modal').addEventListener('click', function(e) {
+    if (e.target.id === 'video-bg') {
+        document.getElementById('video-modal').classList.add('hidden');
+    }
+});
